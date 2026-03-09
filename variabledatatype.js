0aef08c963c13aab0217e2b496fcd5ded9f87e11
suzuuuuuu7 <sujalkc324@gmail.com>
2026-03-09 09:02:54 +0545

    variabledatatype

---
+++ variabledatatype.js
@@ -0,0 +1,38 @@
+console.log("Hello guys"); // use to print code in console
+alert("iam learning javascript"); // use to create popup message
+// name = "suja kc"// bad practise to declare value
+// age  = 45;
+price = 33.33;
+x = null;
+console.log(x);
+y= undefined;
+isfollow = true;
+let name = "sajull"; // let cannot be redeclared but can updated
+let age = 24;
+const address = "Pokhara";
+console.log(address);
+ name = "kanxa";
+ name =33;
+ typeof name;
+console.log(name);
+var weight = 34;
+var weight = "sujal kc";
+console.log(weight);
+typeof weight;
+
+const student = {  // object
+    name:"suja kc",
+    address :"Rolpa",
+    age:24
+}
+console.log(student);
+student["age"]= age+20; // can update value of object even we assigned with const keyword
+const product = {
+    penName:"Parker jotter standard Ct Ball",
+    color:"black",
+    rate:4,
+    price:270,
+    isSell:true,
+    discount:"5%"
+}
+console.log(product);

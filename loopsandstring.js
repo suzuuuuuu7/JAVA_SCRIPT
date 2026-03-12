c6305f65ce6b617e0fb162076eda32b75d6543ec
suzuuuuuu7 <sujalkc324@gmail.com>
2026-03-12 19:40:32 +0545

    loops and string

---
+++ loopsandstring.js
@@ -0,0 +1,32 @@
+//loops
+for(let i =1;i<=5;i++){  // for loop
+    console.log("sujal kc")
+}
+for(let i =2;i<=10;i+=2){
+    console.log(i)
+}
+console.log("loops has ended")
+let sum = 0;
+let n = 6;
+for(i=1;i<=n;i++){
+   sum = sum+i 
+}
+console.log("loop ended");
+console.log(sum);
+console.log(i)
+
+// for(let n=1;n>=2;i++){ //infinite loop
+//     console.log(n)
+
+// }
+// while loop
+let j = 1;
+while(j<=5){
+    console.log(i)
+    j++;
+}
+let a = 1;
+do{
+    console.log("sujal kc")
+    i++
+}while(a<=5);
